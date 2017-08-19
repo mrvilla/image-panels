060d8f39b6e9294ac1cb379d4df8d1e09dd7960a
enrique.cantillo <enrique.cantillo@c02sgjwtg8wp.fritz.box>
2017-08-19 14:31:32 +0200

    images, styles and script

---
+++ dev/scripts/scripts.js
@@ -0,0 +1,32 @@
+/**
+ * Created by enrique.cantillo on 18.08.17.
+ */
+const panels = document.querySelectorAll('.panel');
+const panelState = [];
+
+function updateState() {
+    let currentPanel = this;
+    panels.forEach((panel, i) => {
+        if (currentPanel === panel) {
+            panelState[i] = !panelState[i];
+        } else {
+            panelState[i] = 0;
+        }
+    });
+    toggleState();
+}
+function toggleState() {
+    panels.forEach((panel, i) => {
+        panel.classList.toggle('open', panelState[i])
+    })
+}
+function openText(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('show-text')
+    }
+}
+panels.forEach(panel => panel.addEventListener('click', updateState));
+panels.forEach(panel => panel.addEventListener('transitionend', openText));
+panels.forEach(function(){
+    panelState.push(0);
+});
